288c8e0a4a66c94ae499bfddc58bace4175b6dcf
ValiaMorokina <valmorvidya@gmail.com>
2017-02-02 22:53:36 +0200

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,23 @@
+// Adding Google Maps 
+var myCenter = new google.maps.LatLng(51.5074, -0.1278);
+
+function initialize() {
+var mapProp = {
+center:myCenter,
+zoom:12,
+scrollwheel:false,
+draggable:false,
+mapTypeId:google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker = new google.maps.Marker({
+position:myCenter,
+});
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+//---------------------------------------------------------------------------
